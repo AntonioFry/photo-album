241273929d2711776e4d6a9db1802c45e9565656
Antonio Fry <antoniofry2000@gmail.com>
2020-05-20 12:26:37 -0600

    Export PhotoCard component file

--- src/PhotoCard/PhotoCard.js
+++ src/PhotoCard/PhotoCard.js
@@ -3,8 +3,10 @@ import React from 'react';
 const PhotoCard = ({ photoDescription, imageSrc, photoComment }) => {
   return (
     <article>
-      <img src={''} />
-      <p></p>
+      <img src={imageSrc} alt={photoDescription} />
+      <p>{photoComment}</p>
     </article>
   )
 }
+
+export default PhotoCard;
