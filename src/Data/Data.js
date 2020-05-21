e8f80c0e8382fa18050dc955ff3b380536027da8
Antonio Fry <antoniofry2000@gmail.com>
2020-05-21 11:07:13 -0600

    Create fixed image data set

---
+++ src/Data/Data.js
@@ -0,0 +1,12 @@
+export const imageData = [
+  {
+    imageSrc: require('./Images/ants-and-plants.JPG'),
+    photoComment: 'This is one the first image I took of any sort of insect. After being able to see how much detail that was visible in the image versus the naked eye, I decided to try taking more of the small critters that roam the earth.',
+    photoDescription: 'Red ants crawling over the ball of a soon to bloom flower'
+  },
+  {
+    imageSrc: require('./Images/moon.JPG'),
+    photoComment: "I have always thougth it's always been cool to see the Moon during the day, and so I took a picture. It maybe common photo to find on the web, but it's still cool to see the craters so clearly when it's from a photo you took.",
+    photoDescription: 'The Moon visible among the blue sky'
+  },
+]
