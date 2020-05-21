fb10411650a94ef6ae527851cad24c31c191c84c
Antonio Fry <antoniofry2000@gmail.com>
2020-05-21 11:08:19 -0600

    Mapped and render data in Gallery using PhotoCard component

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import PhotoCard from '../PhotoCard/PhotoCard';
+import { imageData } from '../Data/Data';
 
 const Gallery = () => {
-
+  const photoCards = imageData.map(imgData => {
+    return (
+      <PhotoCard 
+        {...imgData}
+      />
+    )
+  })
   return (
     <section className="gallery-section">
-
+      {photoCards}
     </section>
   )
 }
+
+export default Gallery;
