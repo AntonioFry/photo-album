5649886ce2d584e28dbc61751a8d9d34e1e31bd7
Antonio Fry <antoniofry2000@gmail.com>
2020-05-26 11:33:41 -0600

    Style Gallery layout

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PhotoCard from '../PhotoCard/PhotoCard';
 import { imageData } from '../Data/Data';
+import './Gallery.css';
 
 const Gallery = () => {
   const photoCards = imageData.map(imgData => {
