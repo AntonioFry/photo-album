15efd819774093607c88786b7745730df61a177d
Antonio Fry <antoniofry2000@gmail.com>
2020-06-17 12:25:58 -0600

    Test that PhotoCard renders correctly with given props

--- src/PhotoCard/PhotoCard.test.js
+++ src/PhotoCard/PhotoCard.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PhotoCard from './PhotoCard';
 
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 describe('PhotoCard', () => {
   
   it('Should render without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<PhotoCard imageSrc='some.JPG' photoComment="It's something"/>, div);
+  });
+
+  it('Should render correctly', () => {
+    const {getByTestId} = render(<PhotoCard imageSrc='some.JPG' photoComment="It's a photo of something" photoDescription='A something'/>);
+    expect(getByTestId('photo-comment')).toHaveTextContent("It's a photo of something");
   })
 })
