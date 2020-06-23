0a7b0e071528572a8ac37ce6d3000984745ebeb3
Antonio Fry <antoniofry2000@gmail.com>
2020-06-23 11:49:02 -0600

    Test Gallery to render without crashing

--- src/Gallery/Gallery.test.js
+++ src/Gallery/Gallery.test.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import Gallery from './Gallery';
 import renderer from 'react-test-renderer';
 
-import { render cleanup } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 describe('Gallery', () => {
 
   afterEach(cleanup);
-  
-})
+
+  it('Should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Gallery />, div);
+  });
+
+});
