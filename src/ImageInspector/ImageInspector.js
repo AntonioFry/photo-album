798a36169d37c5bb31a0540c54b3b7be7bc62909
Antonio Fry <antoniofry2000@gmail.com>
2020-08-24 10:57:31 -0600

    Create layout and state of ImageInspector component

---
+++ src/ImageInspector/ImageInspector.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './ImageInspector.css';
+
+class ImageInspector extends Component {
+  constructor() {
+    super();
+    this.state = {
+      imageCollection: [],
+    }
+  }
+};
