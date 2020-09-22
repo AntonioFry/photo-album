8570a418c67b24ae73558deee9a0b214b0d7eb8f
Antonio Fry <antoniofry2000@gmail.com>
2020-09-21 18:05:33 -0600

    Remove ComponentDidUpdate code

--- src/ImageInspector/ImageInspector.js
+++ src/ImageInspector/ImageInspector.js
@@ -7,20 +7,12 @@ class ImageInspector extends Component {
     this.state = {
       imageCollection: props.imageCollection || [],
       currentImageIndex: 0,
-      currentImage: props.startingPhoto
-    }
-  }
-
-  componentDidUpdate(pervProps) {
-    if (this.props.startingPhoto !== pervProps.startingPhoto) {
-      this.setState({ imageCollection: this.props.imageCollection });
-      this.setState({ currentImage: this.props.startingPhoto });
-      this.setState({ currentImageIndex: 0 });
+      currentImage: props.startingPhoto,
     }
   }
 
   changePhoto = async (direction) => {
-    const { imageCollection, currentImage, currentImageIndex } = this.state;
+    const { imageCollection, currentImageIndex } = this.state;
     if (direction === 'left') {
       await this.setState({ currentImageIndex: currentImageIndex - 1 });
       this.setState({ currentImage: imageCollection[this.state.currentImageIndex].imageSrc });
@@ -35,11 +27,21 @@ class ImageInspector extends Component {
 
     return (
       <section className="faded-background">
-        <button className="close-button">X</button>
+        <button className="close-button" onClick={() => this.props.closeInspector()}>X</button>
         <div className="image-container">
-          <button className='left-button' onClick={() => this.changePhoto('left')} disabled={imageCollection[currentImageIndex - 1] === undefined}>{'<'}</button>
+          <button 
+            className='left-button' 
+            onClick={() => this.changePhoto('left')} 
+            disabled={imageCollection[currentImageIndex - 1] === undefined}>
+              {'<'}
+          </button>
           <img className='inspected-image' src={currentImage} alt={currentImage.photoDescription}/>
-          <button className='right-button' onClick={() => this.changePhoto('right')} disabled={imageCollection[currentImageIndex + 1] === undefined}>{'>'}</button>
+          <button
+            className='right-button'
+            onClick={() => this.changePhoto('right')}
+            disabled={imageCollection[currentImageIndex + 1] === undefined}>
+              {'>'}
+          </button>
         </div>
       </section>
     )
