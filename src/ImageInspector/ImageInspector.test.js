21a7eac64eec29332d684f4dd95b0eba03620086
Antonio Fry <antoniofry2000@gmail.com>
2020-11-12 11:23:27 -0700

    Setup ImageInspector test file

---
+++ src/ImageInspector/ImageInspector.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import renderer from 'react-test-renderer';
+
+describe('ImageInspector', () => {
+  
+})
