75e10ade3803edb4237a275ea6fce282072d82e3
Antonio Fry <antoniofry2000@gmail.com>
2021-06-01 08:28:29 -0600

    convert previewImages to a class component

---
+++ src/PreviewImages/PreviewImages.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './PreviewImages.css';
+
+class PreviewImages extends Component {
+  constructor() {
+    super();
+    this.state = {
+      previewAlbum: [],
+      currentPreviewPhoto: ''
+    }
+  }
+
+  render() {
+    return (
+      <section className="preview-image-section">
+        <img src={require('../Data/Images/ants-and-plants.JPG')} />
+      </section>
+    )
+  }
+}
